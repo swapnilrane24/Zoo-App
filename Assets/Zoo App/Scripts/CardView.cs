b5cc121b588a478ec18aeeee1650ad6abb12713a
Swapnil Rane <swapnilrane24@gmail.com>
2019-01-05 11:54:40 +0530

    Complete Zoo App
    Added all the card to the app and scriptable object to increas the app info dynamicly.

---
+++ Assets/Zoo App/Scripts/CardView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    public Text title;
+    public Text description;
+    public Text exhibit;
+    public Image animalImg;
+
+    public CardModel[] cardModels;
+
+    private CardModel targetCard;
+
+    public void DisplayInfo(int index)
+    {
+        title.text = cardModels[index].name;
+        description.text = cardModels[index].description;
+        exhibit.text = cardModels[index].exhibit;
+        animalImg.sprite = cardModels[index].animalImg;
+    }
+
+
+}
---
+++ Assets/Zoo App/Scripts/ScriptableObjects/CardModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Car", menuName ="Cards")]
+public class CardModel : ScriptableObject
+{
+    public string title;
+    [TextArea(3, 5)]
+    public string description;
+    [TextArea(3, 5)]
+    public string exhibit;
+    public Sprite animalImg;
+	
+}
